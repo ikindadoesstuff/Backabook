65ece9a34c76736729936a28e500ec1ce4fe1789
Devyon Lozano <devyonlozano@gmail.com>
2025-04-30 22:01:06 -0600

    feat: mysql connection

---
+++ databaseConnection.php
@@ -0,0 +1,22 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "usbw";
+$database = "bookstoredb";
+
+$connection = new mysqli($host, $user, $pass, $database);
+
+if ($connection->connect_error) {
+    die("Connection Failed: " . $connection->connect_error);
+}
+
+echo "Connected to " . $database . " successfully.";
+
+function queryDB (string $sql) {
+    global $connection;
+    $result = $connection->query($sql);
+    if ($result->num_rows > 0) {
+        return $result;
+    }
+}
+?>
