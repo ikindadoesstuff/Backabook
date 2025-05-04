3ef5ff64ab779fb7af5fbd43b0c256f706a6d17f
Devyon Lozano <devyonlozano@gmail.com>
2025-05-03 20:50:16 -0600

    Revert "failed vercel deployment"
    This reverts commit 53f2c1224d18f6198f0aab64c988e245f6484a12.

--- head.php
+++ head.php
@@ -3,6 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Backabook</title>
     <link href="./style.css" rel="stylesheet" type="text/css">
-    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add,close,production_quantity_limits,remove,remove_shopping_cart,search,shopping_cart" />
 </head>
